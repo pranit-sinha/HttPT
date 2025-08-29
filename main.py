b3ced14903e8a0c31aaa31df31589e65711a4663
jinx <pranitsinha@protonmail.com>
2025-08-29 20:59:11 +0530

    Define registry class

--- main.py
+++ main.py
@@ -1,23 +1,60 @@
 import requests
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from contextlib import asynccontextmanager
 from pydantic import BaseModel
 from transformers import pipeline
 
-models = {}
+class ModelRegistry:
+    def __init__(self):
+        self.backends = {}
+        self.config = {
+                "sentiment-analysis": {
+                    "name": "distilbert-base-uncased-finetuned-sst-2-english",
+                    "task": "sentiment-analysis"
+                    },
+                "image-classification": {
+                    "name": "google/vit-base-patch16-224",
+                    "task": "image-classification"
+                    }
+                }
+
+    def bootstrap(self):
+        for service, config in self.config.items():
+            try:                                                                                                                                                        
+                self.backends[service] = pipeline(task=config["task"], model=config["name"], device=0)
+            except Exception:
+                raise RuntimeError("Failed to load ", service)
+
+    def shutdown(self):
+        self.backends.clear()
+
+models = ModelRegistry()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    models['DistilBert'] = pipeline(model='distilbert-base-uncased-finetuned-sst-2-english')
+    models.bootstrap()
     yield
-    models.clear()
+    models.shutdown()
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(version="0.2.0", lifespan=lifespan)
+
+class InferenceRequest(BaseModel):
+    text: str
+
+class InferenceResponse(BaseModel):
+    service: str
+    preds: list
 
 @app.get("/")
 async def root():
     return {"message": "App running"}
 
-@app.get("/inference")
-async def predict(input: str):
-    return models['DistilBert'](input)[0]
+@app.post("/inference/{service}", response_model=InferenceResponse)
+async def predict(service: str, request: InferenceRequest):
+    model = models.backends.get(service)
+    try:
+        result = model(request.text)
+    except Exception:
+        raise HTTPException(status_code=500, detail="Inference failed.")
+
+    return InferenceResponse(service=service, preds=result if isinstance(result, list) else [result])
